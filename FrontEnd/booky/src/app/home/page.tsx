ba4b1ce8c75dc8bb65a8278139eb311c30725a3c
Kiki <caiquelsobral@gmail.com>
2024-01-13 23:43:15 -0300

    Feat: Add list page and change styles

--- FrontEnd/booky/src/app/home/page.tsx
+++ FrontEnd/booky/src/app/home/page.tsx
@@ -1,3 +1,77 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function HomePage() {
-  return <main></main>;
+  const [selectedStatus, setStatus] = useState('Quero Ler');
+
+  const seila = [
+    {
+      book: 'As crônicas de Nárnia',
+      author: 'Com certeza um author',
+    },
+    { book: 'Harry Potter', author: 'JK Rolando' },
+    { book: 'As crônicas de Silva', author: 'Autor Silva' },
+    { book: 'As crônicas de Sauro', author: 'Autor Sauro' },
+    { book: 'As crônicas de Lopes', author: 'Autor Lopes' },
+    { book: 'As crônicas de Silva', author: 'O Autor da Silva' },
+  ];
+  return (
+    <main className="w-full min-h-[100vh] flex items-center justify-center flex-col gap-16 my-8">
+      {seila.map((item, i) => {
+        return (
+          <div
+            key={i}
+            className="relative w-[800px] h-[300px] bg-purple-700 rounded-xl px-4 py-4 flex items-center justify-center gap-6 shadow-2xl"
+          >
+            <div className="flex-1 w-2/5 h-full  flex flex-col items-center justify-center gap-8">
+              <span className="text-2xl text-purple-100 bg-purple-400 rounded-xl px-4 py-2 font-semibold text-justify">
+                {item.book}
+              </span>
+              <span className="text-2xl text-purple-100 bg-purple-400 rounded-xl px-4 py-2 font-semibold">
+                {item.author}
+              </span>
+            </div>
+            <div className="flex-1 w-2/5 h-full rounded-xl flex flex-col items-center justify-center gap-6 bg-purple-400">
+              {selectedStatus === 'Concluído' ? (
+                <input
+                  type="text"
+                  about="Book Score"
+                  defaultValue={0}
+                  className="text-8xl text-purple-100 bg-purple-400 h-24 w-24 rounded-xl text-center align-middle"
+                ></input>
+              ) : (
+                <input
+                  type="text"
+                  about="Book Score"
+                  disabled
+                  value={'-'}
+                  className="text-8xl text-purple-100 bg-purple-400 h-24 w-24 rounded-xl text-center align-middle"
+                ></input>
+              )}
+
+              <select
+                id="status"
+                className="text-base font-semibold px-2 py-2 bg-purple-700 text-purple-100 rounded-xl"
+                onChange={(item) => {
+                  setStatus(item.target.value);
+                }}
+              >
+                <option about="reading status" value="Quero Ler">
+                  {'Quero ler'}
+                </option>
+                <option about="reading status" value="Lendo">
+                  {'Lendo'}
+                </option>
+                <option about="reading status" value="Concluído">
+                  {'Concluído'}
+                </option>
+              </select>
+            </div>
+            <span className="absolute text-sm rounded-xl px-4 py-2 font-medium text-purple-100 bottom-2 left-2">{`Adicionado em ${'27/07/2001'}`}</span>
+          </div>
+        );
+      })}
+    </main>
+  );
 }
--- FrontEnd/booky/src/app/page.tsx
+++ FrontEnd/booky/src/app/page.tsx
@@ -1,19 +1,23 @@
+'use client';
+
 import { DefaultInput } from '@/components/input';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
+  const router = useRouter();
   return (
     <main className="w-full min-h-[100vh] flex items-center justify-center">
       <section className="flex lg:w-[800px] lg:h-[600px] md:w-[600px] md:h-[500px] bg-purple-800 shadow-xl rounded-xl flex-col justify-center items-center gap-16">
         <DefaultInput type="email" placeholder="E-mail" />
         <DefaultInput type="password" placeholder="Password" />
-        <Link
-          href={'/home'}
+        <button
+          type="button"
           id="btnLogin"
-          className="w-1/4 bg-purple-200 py-4 px-4 text-3xl font-bold uppercase rounded-full text-purple-800 text-center"
+          className="w-1/4 bg-purple-200 py-4 px-4 text-3xl font-bold uppercase rounded-full text-purple-800 text-center hover:bg-purple-300"
+          onClick={() => router.push('/home')}
         >
           Login
-        </Link>
+        </button>
       </section>
     </main>
   );
